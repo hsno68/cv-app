94c814d664754b81f4835599c923c5c75fe4217a
haiseno <haise.zhao@gmail.com>
2025-10-16 00:24:21 -0700

    Create Input component and fields const module

---
+++ src/Input.jsx
@@ -0,0 +1,12 @@
+export default function Input({ type, id, name, required, onChange, ...attributes }) {
+  return (
+    <input
+      type={type}
+      id={id}
+      name={name}
+      required={required}
+      onChange={(e) => onChange(e, name)}
+      {...attributes}
+    />
+  );
+}
---
+++ src/props.js
@@ -0,0 +1,3 @@
+export const personalInfoControls = [
+  { type: "text", label: "First Name", id: "firstName", required: true },
+];
