4e48d02108ecc156f20ac4d469fccc2bc42f392d
haiseno <haise.zhao@gmail.com>
2025-11-05 14:04:16 -0800

    Remove unnecessary fragment

--- src/Select.jsx
+++ src/Select.jsx
@@ -2,23 +2,21 @@ import { majors } from "./config.js";
 
 export default function Select({ id, name, value, required, onChange }) {
   return (
-    <>
-      <select
-        id={id}
-        name={name}
-        value={value}
-        required={required}
-        onChange={onChange}
-      >
-        <option value="" disabled>
-          Select a major
+    <select
+      id={id}
+      name={name}
+      value={value}
+      required={required}
+      onChange={onChange}
+    >
+      <option value="" disabled>
+        Select a major
+      </option>
+      {majors.map((major) => (
+        <option key={major} value={major}>
+          {major}
         </option>
-        {majors.map((major) => (
-          <option key={major} value={major}>
-            {major}
-          </option>
-        ))}
-      </select>
-    </>
+      ))}
+    </select>
   );
 }
